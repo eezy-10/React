215d71435a579ff235fd43fcbe49f770d96e020d
Irtaza Ahmad <ahmadirtaza10@gmail.com>
2025-06-24 17:39:25 +0500

    Day 5 work done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import BookCard from "./BookCard";
 import UserCard from "./UserCard"
 import InputCard from "./InputCard";
+import BookList from "./BookCard";
 
 function App() {
   const appStyle = {
@@ -9,18 +10,19 @@ function App() {
         alignItems: 'center',
         height: '100vh',
         margin: 0,
-        fontFamily: 'Arial, sans-serif',
-        overflow: 'hidden'
+        fontFamily: 'Arial, sans-serif'
   };
 
   return (
     <div style={appStyle}>
       <div>
         {/* <h1>React Prop Example</h1> */}
-        <h1>Handling Input Change</h1>
+        {/* <h1>Handling Input Change</h1> */}
         {/* <UserCard name = 'Irtaza Ahmad' age={22} city = 'Lahore'/> */}
         {/* <BookCard title='The Great Gatsby' author='F. Scott Fitzgerald' year={1925} /> */}
-        <InputCard/>
+        {/* <InputCard/> */}
+        <h1>Rendering Data</h1>
+        <BookList />
       </div>
     </div>    
   );
--- src/BookCard.jsx
+++ src/BookCard.jsx
@@ -1,17 +1,44 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-export default function BookCard({ title, author, year }) {
-    const [count, setcount] = useState(0);
-    const [likes, setlikes] = useState(0);
+// export default function BookCard({ title, author, year }) {
+//     const [count, setcount] = useState(0);
+//     const [likes, setlikes] = useState(0);
+//     return(
+//         <div>
+//             <button onClick={()=>setcount(count+1)}>Increase</button>
+//             <button onClick={()=>setcount(count-1)}>Decrease</button>
+//             <button onClick={()=>setlikes(likes+1)}>Likes {likes}</button>
+//             <h3>Counter is {count}</h3>
+//             <h2>Book Title: {title}</h2>
+//             <h3>Author: {author}</h3>
+//             <h3>Published Year: {year}</h3>
+//         </div>
+//     )
+// }
+
+export default function BookList(){
+    let books = [
+        { title: "The Great Gatsby", author: "F. Scott Fitzgerald"},
+        { title: "To Kill a Mockingbird", author: "Harper Lee"},
+        { title: "1984", author: "George Orwell"},
+        { title: "Pride and Prejudice", author: "Jane Austen"}
+    ];
+
     return(
-        <div>
-            <button onClick={()=>setcount(count+1)}>Increase</button>
-            <button onClick={()=>setcount(count-1)}>Decrease</button>
-            <button onClick={()=>setlikes(likes+1)}>Likes {likes}</button>
-            <h3>Counter is {count}</h3>
-            <h2>Book Title: {title}</h2>
-            <h3>Author: {author}</h3>
-            <h3>Published Year: {year}</h3>
-        </div>
-    )
+        books.length > 0 ? (
+            <div>
+                <h2>Books List</h2>
+                <ul>
+                    {books.map((book, index) => (
+                        <li key={index}>Title: {book.title}, Author: {book.author}</li>
+                    ))}
+                </ul>
+                <p>Total books: {books.length}</p>
+            </div>
+        ) : (
+            <div>
+                <p>No Books found.</p>
+            </div>
+        )
+    );
 }
